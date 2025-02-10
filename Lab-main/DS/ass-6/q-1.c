46e25d8d3938459d7b6b51992b68188e0d34a7dd
Shravya PAtel <shravyapatel2806@gmail.com>
2025-02-10 06:44:26 +0000

    work

--- Lab-main/DS/ass-6/q-1.c
+++ Lab-main/DS/ass-6/q-1.c
@@ -30,39 +30,42 @@ void intial_elements(node **head)
             y = 0;
             break;
         default:
-        printf("Try again\n");
+            printf("Try again\n");
         }
     }
 }
-void print_stack(node **head){
+void print_stack(node **head)
+{
     node *temp = *head;
-    while(temp!=NULL){
-        printf("%d\n",temp->data);
-        printf("|\n");
+    while (temp != NULL)
+    {
+        printf("%d->", temp->data);
         temp = temp->pre;
     }
     printf("NULL\n");
 }
-void insert(node **head){
+void insert(node **head)
+{
     node *temp = *head;
     node *newnode = NULL;
     int y = 1;
-    while(y){
+    while (y)
+    {
         printf("Enter 1 to insert new data or else enter 2: ");
         int condition;
-        scanf("%d",&condition);
+        scanf("%d", &condition);
         switch (condition)
         {
         case 1:
             newnode = (node *)malloc(sizeof(node));
             printf("Enter the data: ");
-            scanf("%d",&(newnode)->data);
+            scanf("%d", &(newnode)->data);
             newnode->pre = temp;
             *head = newnode;
             temp = newnode;
             break;
         case 2:
-            y=0;
+            y = 0;
             break;
         default:
             printf("Try again\n");
@@ -70,10 +73,43 @@ void insert(node **head){
         }
     }
 }
-int main(){
+void pop(node **head)
+{
+    node *temp = *head, *temp1;
+    int y = 1;
+    while (y)
+    {
+        printf("Enter 1 to pop the element else enter 2 to exit: ");
+        int condition;
+        scanf("%d", &condition);
+
+        switch (condition)
+        {
+        case 1:
+            temp = temp1;
+            if (temp->pre == NULL)
+            {
+                printf("%d->NULL", (temp->data));
+                return;
+            }
+            /* code */
+            *head = temp->pre;
+            temp1 = temp->pre;
+            free(temp);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+int main()
+{
     node *head = NULL;
     intial_elements(&head);
     print_stack(&head);
+    printf("Insert: \n");
     insert(&head);
     print_stack(&head);
+    pop(&head);
 }
