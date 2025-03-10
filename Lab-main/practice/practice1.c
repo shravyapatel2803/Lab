fc4998d3adb4f47deecc71dff642095285ec0cac
Shravya Patel <shravyapatel2806@gmail.com>
2025-03-10 05:06:27 +0000

    work

--- Lab-main/practice/practice1.c
+++ Lab-main/practice/practice1.c
@@ -1,8 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 100
+
+struct lifo
+{
+    int st[MAXSIZE];
+    int top;
+};
+
+typedef struct lifo
+
+    stack;
+
+stack s;
+void create(stack *s)
+{
+    s->top = -1;
+}
+void push(stack *s,int element)
+{
+    if (s->top == (MAXSIZE - 1))
+
+    {
+
+        printf("\nStackoverflow");
+
+        exit(-1);
+    }
+
+    else
+
+    {
+
+        s->top++;
+
+        s->st[s->top] = element;
+    }
+}
 int main(){
-    int n;
-    scanf("%d",&n);
-    printf("%d",n);
-    return 0;
+    stack *s ;
+    create(s);
+    push(s,12);
 }
